2f9c62b4434a0f8aa449400a352b526c08b6c45d
Derek Stone <derekstone@unr.edu>
2024-12-11 05:15:02 +0000

    code now had comments and is ready for final submission

--- madlibs.c
+++ madlibs.c
@@ -8,7 +8,7 @@
 #define WORD_CAP 30
 #define FILE_NAME "madlib2.txt"
 
-//function prototypes
+//Function prototypes
 void arrayLength(FILE* fptr, int* length);
 void fillArray(int length, char data[][STRING_CAPACITY], FILE* fptr);
 void addWords(char data[][STRING_CAPACITY], int length);
@@ -44,7 +44,7 @@ int main(){
 }
 
 
-
+//This function gets the length of our file.
 void arrayLength(FILE* fptr, int* length){
 	char line[STRING_CAPACITY];
 	while (fgets(line, STRING_CAPACITY, fptr) != 0){
@@ -52,7 +52,7 @@ void arrayLength(FILE* fptr, int* length){
 	}
 }
 
-
+//This function copies the file into a 2D array, nowe that we have the length of the file.
 void fillArray(int length, char data[][STRING_CAPACITY], FILE* fptr){
 	for(int row = 0; row <= length; row++){
 		fgets(data[row], STRING_CAPACITY, fptr);
@@ -64,7 +64,7 @@ void fillArray(int length, char data[][STRING_CAPACITY], FILE* fptr){
 	}
 }
 
-
+//This function scans the 2D array for all letters that will need to be replaced, but runs function grabUserWord to actually swap out the letters.
 void addWords(char data[][STRING_CAPACITY], int length){
 	for (int row = 0; row < length; row++){
 		if (data[row][2] == '\0'){
@@ -73,7 +73,7 @@ void addWords(char data[][STRING_CAPACITY], int length){
 	}
 }
 
-
+//This function takes in the letter and asks the user for the approriate kind of word to replace it.
 void grabUserWord(char letter, char word[WORD_CAP]){
 	switch(letter){
 		case 'A':
@@ -93,7 +93,7 @@ void grabUserWord(char letter, char word[WORD_CAP]){
 }
 
 
-//Print Madlibs
+//Here we reprint the 2D array now that we have replaced the blank word spots with user inputted words. It prints out like an organized paragrpah, adding spaces where needed to our replaced words.
 void print2DArray(int num_rows, char data[][STRING_CAPACITY]){
 	for(int row = 0; row < num_rows; row++){
 		for(int col = 0; data[row][col] != '\0'; col++){
